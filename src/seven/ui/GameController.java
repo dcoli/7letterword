544cea40f6bfce6328993beaac691f2f743ffc19
ma.danbo <ma.danbo@4f143674-d4ba-11de-bb02-e302c5db49d5>
2009-11-21 22:01:12 +0000

    version 1.6 of the simulator

--- src/seven/ui/GameController.java
+++ src/seven/ui/GameController.java
@@ -39,7 +39,7 @@ public class GameController {
             {
 
                 Player currPlayer = gc_local.PObjectList.get(loop);
-                int bidValue = currPlayer.Bid(bidLetter,gc_local.BidList,gc_local.number_of_rounds,gc_local.PlayerList);
+                int bidValue = currPlayer.Bid(bidLetter,gc_local.BidList,gc_local.number_of_rounds,gc_local.PlayerList,gc_local.secretstateList.get(loop));
                 // If player is full makes his value = 0.
                 if(bidValue < 0)
                 {
