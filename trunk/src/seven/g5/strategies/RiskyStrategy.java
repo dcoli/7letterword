c9ab3ce128838e64e96f79c63d6b2c200c9b1310
comedyboy19 <comedyboy19@4f143674-d4ba-11de-bb02-e302c5db49d5>
2009-11-30 03:12:46 +0000

    adding risky skeleton

---
+++ trunk/src/seven/g5/strategies/RiskyStrategy.java
@@ -0,0 +1,25 @@
+package seven.g5.strategies;
+
+import java.util.ArrayList;
+
+import seven.ui.Letter;
+import seven.ui.PlayerBids;
+import seven.ui.SecretState;
+
+public class RiskyStrategy extends Strategy {
+
+	@Override
+	public int getBid(Letter bidLetter, ArrayList<PlayerBids> playerBidList,
+			int totalRounds, ArrayList<String> playerList,
+			SecretState secretstate, int playerID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getFinalWord() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
