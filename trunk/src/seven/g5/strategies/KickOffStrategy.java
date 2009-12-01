7049191cfb90ca519a7dea03f38e26991f071814
comedyboy19 <comedyboy19@4f143674-d4ba-11de-bb02-e302c5db49d5>
2009-12-01 07:32:02 +0000

    added kickoff skeleton

---
+++ trunk/src/seven/g5/strategies/KickOffStrategy.java
@@ -0,0 +1,19 @@
+package seven.g5.strategies;
+
+import seven.g5.gameHolders.GameInfo;
+import seven.g5.gameHolders.PlayerInfo;
+
+public class KickOffStrategy extends Strategy {
+
+	@Override
+	public int getBid(GameInfo gi, PlayerInfo pi) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getFinalWord() {
+		return "balls";
+	}
+
+}
