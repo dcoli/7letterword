62210cc6cafa7dbbb5f0076f08175d953d786d6c
ma.danbo <ma.danbo@4f143674-d4ba-11de-bb02-e302c5db49d5>
2009-12-02 02:59:46 +0000

    initial

--- src/seven/g5/strategies/KickOffStrategy.java
+++ src/seven/g5/strategies/KickOffStrategy.java
@@ -2,6 +2,7 @@ package seven.g5.strategies;
 
 import seven.g5.gameHolders.GameInfo;
 import seven.g5.gameHolders.PlayerInfo;
+import seven.g5.Utilities;
 
 public class KickOffStrategy extends Strategy {
 
@@ -12,8 +13,12 @@ public class KickOffStrategy extends Strategy {
 
 	@Override
 	public int getBid(GameInfo gi, PlayerInfo pi) {
-		// TODO Auto-generated method stub
+		if ( pi.getRack().size() == 0 || pi.getRack().size() == 1 ) {
+			if( 	(Utilities.getProbabilityOfLetter(gi.getCurrentBidLetter(),gi) < .20f) &&
+					(Utilities.getProbabilityOfLetter(gi.getCurrentBidLetter(),gi) > .05f)) {
+				return gi.getCurrentBidLetter().getValue();
+			}
+		}
 		return 0;
 	}
-
 }
