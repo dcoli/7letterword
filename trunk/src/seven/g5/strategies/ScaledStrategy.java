54ed94808146c9ab13ac3597d48e42a7b7eac022
ma.danbo <ma.danbo@4f143674-d4ba-11de-bb02-e302c5db49d5>
2009-12-02 05:49:15 +0000

    kickoff is working

---
+++ trunk/src/seven/g5/strategies/ScaledStrategy.java
@@ -0,0 +1,24 @@
+package seven.g5.strategies;
+
+import seven.g5.gameHolders.GameInfo;
+import seven.g5.gameHolders.PlayerInfo;
+
+public class ScaledStrategy extends Strategy {
+
+	public ScaledStrategy() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getBid(GameInfo gi, PlayerInfo pi) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getFinalWord() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
