68a66f0f2e97b891bba297296f4d8831d1e5a6b0
monalsanghvi <monalsanghvi@4f143674-d4ba-11de-bb02-e302c5db49d5>
2009-12-02 02:45:50 +0000

    static method for probability

--- trunk/src/seven/g5/Utilities.java
+++ trunk/src/seven/g5/Utilities.java
@@ -2,16 +2,11 @@ package seven.g5;
 
 import seven.g5.gameHolders.*;
 import seven.ui.*;
-import java.util.*;
 
 public class Utilities {
-
-	public HashMap<Character, Integer> numberLettersRemaining;
-	int sumofLettersRemaining;
 	
-	public float getProbabilityOfLetter(Letter letter1, GameInfo gi)	
+	public static float getProbabilityOfLetter(Letter letter1, GameInfo gi)	
 	{
-		numberLettersRemaining = gi.getNumberLettersRemaining();
-		return numberLettersRemaining.get(letter1.getAlphabet())/gi.getNoOfTurnsRemaining();		
+		return gi.getNumberLettersRemaining().get(letter1.getAlphabet())/gi.getNoOfTurnsRemaining();		
 	}
 }
